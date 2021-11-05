c5653dc48c1940cac7f0f27fe611b0c7474e05c6
miirsery <miirsery.main@gmail.com>
2021-11-05 17:59:46 +0700

    add new functional

--- frontend/js/cart.js
+++ frontend/js/cart.js
@@ -4,20 +4,20 @@ const itemsBtnImgDisable = document.querySelectorAll('.content__favorite-img-dis
 const favoriteShowListBtn = document.querySelector('.favorite-btn');
 const favorite = document.querySelector('.favorite');
 const deleteBtn = document.querySelector('.favorite-modal__item-delete');
-
 const randomId = () => {
     return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 };
 
-const generateCartProduct = (img, title, id) => {
+const generateCartProduct = (img, title, id, description) => {
     return `
-    <li class="favorite-modal__item" data-id="${id}>
+    <li class="favorite-modal__item" data-id="${id}">
     <div class="favorite-modal-left">
-        <img class="favorite-modal__item-img" src="${img}" alt=""">
+        <img class="favorite-modal__item-img" src="${img}" alt="">
         <h5 class="favorite-modal__item-title"> ${title}</h5>
     </div>
+    <p class="content__description" style="display:block">${description}</p>
     <button class="favorite-modal__item-delete" aria-label="Удалить">Удалить</button>
-</li>
+    </li>
 	`;
 };
 
@@ -33,37 +33,44 @@ itemsBtn.forEach(el => {
         let itemsBtnImgDisable = el.querySelector('.content__favorite-img-disable');
         let self = e.currentTarget;
         let parent = self.closest('.content__link');
-        let child = self.closest('.favorite-modal__item');
         let id = parent.dataset.id;
         let img = parent.querySelector('.content__img').getAttribute('src');
         let title = parent.querySelector('.content__title').textContent;
-
-
-
+        let description = parent.querySelector('.content__description').textContent;
+        let deleteBtn = document.querySelector('.favorite-modal__item-delete');
 
         itemsBtnImg.style.display = (itemsBtnImg.style.display == 'none') ? 'block' : 'none';
         itemsBtnImgDisable.style.display = (itemsBtnImgDisable.style.display == 'block') ? 'none' : 'block';
-        if (itemsBtnImgDisable.style.display == 'block') {
-            console.log('actove')
-            favoriteList.insertAdjacentHTML('afterbegin', generateCartProduct(img, title, id));
-            if (favoriteList.childElementCount >= 1)
-                favoriteShowListBtn.style.pointerEvents = 'all';
 
+        if (itemsBtnImgDisable.style.display == 'block') {
+            favoriteList.insertAdjacentHTML('afterbegin', generateCartProduct(img, title, id, description));
+            removeProduct(itemsBtnImgDisable, itemsBtnImg);
+            console.log(favoriteList.childElementCount);
+            if (favoriteList.childElementCount >= 1)
+                favoriteShowListBtn.style.pointerEvents = 'all'
+            if (favoriteList.childElementCount <= 0)
+                favoriteShowListBtn.style.pointerEvents = 'none';
         }
         else {
-            console.log('Imagine that it`s deleted');
+            itemsBtnImgDisable.style.display = 'none';
+            if (deleteBtn.classList.contains('favorite-modal__item-delete')) {
+                removeFilm(deleteBtn.closest('.favorite-modal__item'));
+                console.log(favoriteList.childElementCount);
+            }
         }
-
     });
 });
 
 
-favoriteList.addEventListener('click', (e) => {
-    if (e.target.classList.contains('favorite-modal__item-delete')) {
-        console.log(e.target);
-        removeFilm(e.target.closest('.favorite-modal__item'));
 
-        if (favoriteList.childElementCount == 0)
-            favoriteShowListBtn.style.pointerEvents = 'none';
-    }
-})
+
+function removeProduct(arg, arg2) {
+    favoriteList.addEventListener('click', (e) => {
+        arg.style.display = 'none';
+        arg2.style.display = 'block';
+        if (e.target.classList.contains('favorite-modal__item-delete')) {
+            console.log('Deleted with button')
+            removeFilm(e.target.closest('.favorite-modal__item'));
+        }
+    })
+}
---
+++ frontend/js/singleselect.js
@@ -0,0 +1,8 @@
+const dropdownBtn = document.querySelectorAll('.filters-main__item');
+
+dropdownBtn.forEach(el => {
+    el.addEventListener('click', (e) => {
+        let dropdown = el.querySelector('.dropdown');
+        dropdown.classList.toggle('dropdown-active');
+    })
+})
